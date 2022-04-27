6061c3ef21354821b171a2e29818326273cb82a5
AaroKinnunen <91610235+AaroKinnunen@users.noreply.github.com>
2022-04-27 11:13:35 +0300

    background (#11)
    Some background changes

--- app/src/main/java/com/example/arduino_sense/StartActivity.kt
+++ app/src/main/java/com/example/arduino_sense/StartActivity.kt
@@ -1,15 +1,19 @@
 package com.example.arduino_sense
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import kotlin.system.exitProcess
 
@@ -18,10 +22,12 @@ var data = AppData()
 class StartActivity : AppCompatActivity() {
     private lateinit var openLogin: Button
     private lateinit var openSignup: Button
+    private lateinit var usertext:TextView
     lateinit var pref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this,R.color.trans)))
         pref = this.getSharedPreferences("token", Context.MODE_PRIVATE)
         setContentView(R.layout.start_activity)
         readUser()
@@ -75,12 +81,18 @@ class StartActivity : AppCompatActivity() {
         loginOrLogoutText()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun readUser() {
         var gson = Gson()
         val jsonUser = pref.getString("user", "{ 'username': '', 'token': ''}")
         val loggedUser: LoggedUser = gson.fromJson(jsonUser, LoggedUser::class.java)
         data.setUsername(loggedUser.username)
         data.setToken(loggedUser.token)
+        usertext=findViewById(R.id.user)
+        if (data.getUsername().isNotEmpty()) {
+            usertext.text= "Logged in as: ${data.getUsername()}"
+        }
+        else{usertext.text=""}
     }
     private fun loginOrConnect(){
         if (data.getToken().startsWith("Bearer")) {
